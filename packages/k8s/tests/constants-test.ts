feb9c579c1fa29ad409388649579401e61520652
Nikola Jokic <nikola-jokic@github.com>
2022-06-21 16:55:36 +0200

    added getVolumeClaimName test

--- packages/k8s/tests/constants-test.ts
+++ packages/k8s/tests/constants-test.ts
@@ -1,4 +1,8 @@
-import { getJobPodName, getRunnerPodName } from '../src/hooks/constants'
+import {
+  getJobPodName,
+  getRunnerPodName,
+  getVolumeClaimName
+} from '../src/hooks/constants'
 
 describe('constants', () => {
   describe('getRunnerPodName', () => {
@@ -33,13 +37,43 @@ describe('constants', () => {
           expect: 'test-workflow'
         },
         {
-          // 'a' * 63
+          // podName.length == 63
           podName:
             'abcdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
           expect:
             'abcdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-workflow'
         }
       ]
+
+      for (const tt of tableTests) {
+        process.env.ACTIONS_RUNNER_POD_NAME = tt.podName
+        const got = getJobPodName()
+        expect(got).toBe(tt.expect)
+      }
+    })
+  })
+
+  describe('getVolumeClaimName', () => {
+    it('should throw on getJobPodName if ACTIONS_RUNNER_POD_NAME env is not set', () => {
+      delete process.env.ACTIONS_RUNNER_CLAIM_NAME
+      delete process.env.ACTIONS_RUNNER_POD_NAME
+      expect(() => getVolumeClaimName()).toThrow()
+
+      process.env.ACTIONS_RUNNER_POD_NAME = ''
+      expect(() => getVolumeClaimName()).toThrow()
+    })
+
+    it('should return ACTIONS_RUNNER_CLAIM_NAME env if set', () => {
+      const claimName = 'testclaim'
+      process.env.ACTIONS_RUNNER_CLAIM_NAME = claimName
+      process.env.ACTIONS_RUNNER_POD_NAME = 'example'
+      expect(getVolumeClaimName()).toBe(claimName)
+    })
+
+    it('should contain suffix -work if ACTIONS_RUNNER_CLAIM_NAME is not set', () => {
+      delete process.env.ACTIONS_RUNNER_CLAIM_NAME
+      process.env.ACTIONS_RUNNER_POD_NAME = 'example'
+      expect(getVolumeClaimName()).toBe('example-work')
     })
   })
 })
