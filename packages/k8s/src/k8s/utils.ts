3b349b061cb6159c658afc46312fff35838b21aa
DongHo Jung <dongho971220@gmail.com>
2023-12-12 11:56:50 +0900

    add ENV_USE_HOST_VOLUME

--- packages/k8s/src/k8s/utils.ts
+++ packages/k8s/src/k8s/utils.ts
@@ -14,6 +14,7 @@ export const DEFAULT_CONTAINER_ENTRY_POINT = 'tail'
 
 export const ENV_HOOK_TEMPLATE_PATH = 'ACTIONS_RUNNER_CONTAINER_HOOK_TEMPLATE'
 export const ENV_USE_KUBE_SCHEDULER = 'ACTIONS_RUNNER_USE_KUBE_SCHEDULER'
+export const ENV_USE_HOST_VOLUME = 'ACTIONS_RUNNER_USE_HOST_VOLUME'
 
 export function containerVolumes(
   userMountVolumes: Mount[] = [],
