b05a88f98530c7acdf44cc871b1895ea8ebb3617
John Dreyer <john.dreyer@fortescue.com>
2025-03-12 17:30:54 +0800

    feat: label job pods with context info

--- packages/k8s/src/k8s/index.ts
+++ packages/k8s/src/k8s/index.ts
@@ -1,6 +1,10 @@
 import * as core from '@actions/core'
+<<<<<<< HEAD
 import * as path from 'path'
 import { spawn } from 'child_process'
+=======
+import { context as jobContext } from '@actions/github'
+>>>>>>> a3d6427 (feat: label job pods with context info)
 import * as k8s from '@kubernetes/client-node'
 import tar from 'tar-fs'
 import * as stream from 'stream'
@@ -62,8 +66,38 @@ export const requiredPermissions = [
   }
 ]
 
+<<<<<<< HEAD
 export async function createJobPod(
   name: string,
+=======
+/**
+ * Valid k8s labels conform to the following rules:
+ *  - must be 63 characters or less (can be empty),
+ *  - unless empty, must begin and end with an alphanumeric character ([a-z0-9A-Z]),
+ *  - could contain dashes (-), underscores (_), dots (.), and alphanumerics between
+ * @param label to be sanitized
+ * @returns sanitized label
+ */
+function sanitizeLabel(label: string): string {
+  const sluggedLabel = label
+    .replace(/[^a-zA-Z0-9\-_.]/g, '') // Strip disallowed characters
+    .replace(/^[^a-zA-Z0-9]+/, '') // Ensure it starts with an alphanumeric character
+    .replace(/[^a-zA-Z0-9]+$/, '') // Ensure it ends with an alphanumeric character
+    .substring(0, 63) // Truncate to 63 characters
+
+  const kubernetesLabelRegex = /^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$/
+  if (sluggedLabel !== '' && !kubernetesLabelRegex.test(sluggedLabel)) {
+    core.warning(
+      `Sanitized label "${sluggedLabel}" does not match Kubernetes label regex. Original: "${label}", ignoring label.`
+    )
+    return ''
+  }
+
+  return sluggedLabel
+}
+
+export async function createPod(
+>>>>>>> a3d6427 (feat: label job pods with context info)
   jobContainer?: k8s.V1Container,
   services?: k8s.V1Container[],
   registry?: Registry,
@@ -85,10 +119,30 @@ export async function createJobPod(
   appPod.metadata = new k8s.V1ObjectMeta()
   appPod.metadata.name = name
 
+  const { GITHUB_RUN_ID, GITHUB_RUN_NUMBER, GITHUB_RUN_ATTEMPT } = process.env
+  const arcLabels = Object.fromEntries(
+    Object.entries({
+      'arc-context-event-name': jobContext.eventName,
+      'arc-context-sha': jobContext.sha,
+      'arc-context-workflow': jobContext.workflow,
+      'arc-context-actor': jobContext.actor,
+      'arc-context-job': jobContext.job,
+      'arc-context-repository': jobContext.repo.repo,
+      'arc-context-repository-owner': jobContext.repo.owner,
+      'arc-context-run-id': GITHUB_RUN_ID || '',
+      'arc-context-run-number': GITHUB_RUN_NUMBER || '',
+      'arc-context-run-attempt': GITHUB_RUN_ATTEMPT || ''
+    }).map(([key, value]) => [key, sanitizeLabel(value)])
+  )
+
   const instanceLabel = new RunnerInstanceLabel()
   appPod.metadata.labels = {
-    [instanceLabel.key]: instanceLabel.value
+    [instanceLabel.key]: instanceLabel.value,
+    ...arcLabels
   }
+
+  core.debug(`Pod labels: ${JSON.stringify(appPod.metadata.labels)}`)
+
   appPod.metadata.annotations = {}
 
   appPod.spec = new k8s.V1PodSpec()
