88dc98f8ef83fe01855a7b67ed068f5c8353e8d5
Andre Klärner <kandre+github@ak-online.be>
2025-04-14 14:03:05 +0200

    k8s: start logging from the beginning (#184)

--- packages/k8s/src/k8s/index.ts
+++ packages/k8s/src/k8s/index.ts
@@ -457,7 +457,6 @@ export async function getPodLogs(
 
   const r = await log.log(namespace(), podName, containerName, logStream, {
     follow: true,
-    tailLines: 50,
     pretty: false,
     timestamps: false
   })
