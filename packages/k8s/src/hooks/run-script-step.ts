7efaa9a023f7a509d5fbb265a9097b75e6f13de3
gi1tee <gouzhonglin@huawei.com>
2025-05-12 10:59:17 +0800

    check-npu

--- packages/k8s/src/hooks/run-script-step.ts
+++ packages/k8s/src/hooks/run-script-step.ts
@@ -20,6 +20,22 @@ export async function runScriptStep(
     environmentVariables
   )
 
+  try {
+    await execPodStep(
+      [
+        '/bin/bash',
+        '-el',
+        `/check-npu/check.sh`
+      ],
+      state.jobPod,
+      JOB_CONTAINER_NAME
+    )
+  } catch (err) {
+    core.debug(`execPodStep failed: ${JSON.stringify(err)}`)
+    const message = (err as any)?.response?.body?.message || err
+    throw new Error(`failed to run script step: ${message}`)
+  }
+
   args.entryPoint = 'sh'
   args.entryPointArgs = ['-e', containerPath]
   try {
