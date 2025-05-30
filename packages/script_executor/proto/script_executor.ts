a9bb001581adf6b2037fb4499da8e37496b3a61b
Quoc Truong <quoct@google.com>
2025-05-30 13:57:02 -0700

    Add proto file for GRPC to execute script in a workflow pod. Also add the script executor server that will be used to execute script in the workflow pod.

---
+++ packages/script_executor/proto/script_executor.ts
@@ -0,0 +1,17 @@
+import type * as grpc from '@grpc/grpc-js';
+import type { MessageTypeDefinition } from '@grpc/proto-loader';
+
+import type { ScriptExecutorClient as _script_executor_ScriptExecutorClient, ScriptExecutorDefinition as _script_executor_ScriptExecutorDefinition } from './script_executor/ScriptExecutor';
+
+type SubtypeConstructor<Constructor extends new (...args: any) => any, Subtype> = {
+  new(...args: ConstructorParameters<Constructor>): Subtype;
+};
+
+export interface ProtoGrpcType {
+  script_executor: {
+    ScriptExecutor: SubtypeConstructor<typeof grpc.Client, _script_executor_ScriptExecutorClient> & { service: _script_executor_ScriptExecutorDefinition }
+    ScriptRequest: MessageTypeDefinition
+    ScriptResponse: MessageTypeDefinition
+  }
+}
+
---
+++ packages/script_executor/proto/script_executor/ScriptResponse.ts
@@ -0,0 +1,20 @@
+// Original file: ../proto/script_executor.proto
+
+
+export interface ScriptResponse {
+  'output'?: (string);
+  'error'?: (string);
+  'code'?: (number);
+  '_output'?: "output";
+  '_error'?: "error";
+  '_code'?: "code";
+}
+
+export interface ScriptResponse__Output {
+  'output'?: (string);
+  'error'?: (string);
+  'code'?: (number);
+  '_output'?: "output";
+  '_error'?: "error";
+  '_code'?: "code";
+}
